6b57a51a0ab2e2375cfd0cac9da21721d7b9563c
Ewcia100 <epawelska@gmail.com>
2017-11-15 13:30:06 +0100

    Task 7.1. Upiększacz tekstów

--- src/main/java/com/kodilla/stream/StreamMain.java
+++ src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,7 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -10,26 +12,46 @@ public class StreamMain {
 //        SaySomething saySomething = new SaySomething();
 //        saySomething.say();
 
-        Processor processor = new Processor();
-        //ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        //processor.execute(executeSaySomething);
-        //Executor codeToExecute = () -> System.out.println("This is an example text.");
-        //processor.execute(codeToExecute);
-        processor.execute(() -> System.out.println("This is an example text."));
+//        Processor processor = new Processor();
+//        //ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+//        //processor.execute(executeSaySomething);
+//        //Executor codeToExecute = () -> System.out.println("This is an example text.");
+//        //processor.execute(codeToExecute);
+//        processor.execute(() -> System.out.println("This is an example text."));
+//
+//        //Przykład z kalkulatorem
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//        System.out.println("Calculating expressions with lambdas");
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//
+//        System.out.println("Calculating expressions with method references");
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        //Przykład z kalkulatorem
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Przykładowy tekst", String::toUpperCase);
+        poemBeautifier.beautify("Przykładowy tekst", text -> "<<< " + text + " >>>");
+        poemBeautifier.beautify("Przykładowy tekst", text -> {
+            String result = "";
+            for (char a : text.toCharArray()) {
+                result += a + " ";
+            }
+            return result;
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        });
+        poemBeautifier.beautify("Przykładowy tekst", text -> {
+            String textPartOne=text.substring(0,12).toUpperCase();
+            String textPartTwo = text.substring(12).toLowerCase();
+            return textPartOne+textPartTwo;
+
+        });
+
+
     }
 
 
