47db888edb4d71a628948eaa6c2796b726a9c461
Ewcia100 <epawelska@gmail.com>
2017-11-16 10:57:37 +0100

    Stream:examples from Kodilla Part2

--- src/main/java/com/kodilla/stream/StreamMain.java
+++ src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,61 +1,73 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.person.People;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-//        System.out.println("Welcome to module 7 - Stream");
+//        People.getList().stream()
+//                .forEach(System.out::println);
+//        People.getList().stream()
+//                .map(s -> s.toUpperCase())
+//                .forEach(System.out::println);
+//        People.getList().stream()
+//                .map(String::toUpperCase)
+//                .forEach(s -> System.out.println(s));
+//        System.out.println("Wyświetlenie listy Stringów dłuższych niż 11 znaków ");
+//        People.getList().stream()
+//                .filter(s -> s.length() > 11)
+//                .forEach(System.out::println);
+//        //Wyświetlanie listy Stringów przetransformowanych na duże litery, dłuższych niż 11 znaków, gdy zostanie
+//                wzięta 1 litera nazwiska , odrzucone, te których pierwszą literą imienia jest M"
+//        People.getList().stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.length() > 11)
+//                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+//                .filter(s -> s.substring(0, 1).equals("M"))
+//                .forEach(System.out::println);
+
+        //Do klasy o książce
+//        BookDirectory theBookDirectory = new BookDirectory();
+//        theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .forEach(System.out::println);
+
+        //Kolektory .toList()
+//        BookDirectory theBookDirectory = new BookDirectory();
+//        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .collect(Collectors.toList());
 //
-//        SaySomething saySomething = new SaySomething();
-//        saySomething.say();
+//        System.out.println("# elements: " + theResultListOfBooks.size());
+//        theResultListOfBooks.stream()
+//                .forEach(System.out::println);
 
-//        Processor processor = new Processor();
-//        //ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-//        //processor.execute(executeSaySomething);
-//        //Executor codeToExecute = () -> System.out.println("This is an example text.");
-//        //processor.execute(codeToExecute);
-//        processor.execute(() -> System.out.println("This is an example text."));
+        //Kolektory .toMap()
+//        BookDirectory theBookDirectory = new BookDirectory();
 //
-//        //Przykład z kalkulatorem
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//        System.out.println("Calculating expressions with lambdas");
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+//        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .collect(Collectors.toMap(Book::getSignature, book -> book));
 //
-//        System.out.println("Calculating expressions with method references");
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+//        System.out.println("# elements: " + theResultMapOfBooks.size());
+//        theResultMapOfBooks.entrySet().stream()
+//                .map(entry -> entry.getKey() + ": " + entry.getValue())
+//                .forEach(System.out::println);
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("Przykładowy tekst", String::toUpperCase);
-        poemBeautifier.beautify("Przykładowy tekst", text -> "<<< " + text + " >>>");
-        poemBeautifier.beautify("Przykładowy tekst", text -> {
-            String result = "";
-            for (char a : text.toCharArray()) {
-                result += a + " ";
-            }
-            return result;
+        //Kolektory .joining
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-        });
-        poemBeautifier.beautify("Przykładowy tekst", text -> {
-            String textPartOne=text.substring(0,12).toUpperCase();
-            String textPartTwo = text.substring(12).toLowerCase();
-            return textPartOne+textPartTwo;
-
-        });
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
-
+        System.out.println(theResultStringOfBooks);
     }
 
 
